125ab952e9c9122202138ecf0c6fca00075cddf4
rolik55 <65602933+rolik55@users.noreply.github.com>
2022-07-25 12:11:36 +0300

    euro class unit tests

---
+++ test/java/mjc/EuroTest.java
@@ -0,0 +1,40 @@
+package test.java.mjc;
+
+import main.java.mjc.Euro;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EuroTest {
+
+    @Test
+    void add() {
+        Euro euro1 = new Euro(123);
+        Euro euro2 = new Euro(3685);
+        String expected = "38.8";
+        assertEquals(expected, euro1.add(euro2).toString());
+    }
+
+    @Test
+    void sub() {
+        Euro euro1 = new Euro(123);
+        Euro euro2 = new Euro(3685);
+        String expected = "35.62";
+        assertEquals(expected, euro2.sub(euro1).toString());
+    }
+
+    @Test
+    void mul() {
+        Euro euro = new Euro(123);
+        int k = 8;
+        String expected = "9.84";
+        assertEquals(expected, euro.mul(k).toString());
+    }
+
+    @Test
+    void testToString() {
+        Euro euro = new Euro(65278);
+        String expected = "652.78";
+        assertEquals(expected, euro.toString());
+    }
+}

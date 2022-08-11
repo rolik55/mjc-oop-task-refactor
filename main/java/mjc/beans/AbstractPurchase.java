f8d2049dc99d97eece26c29fa95933c733ab0e46
rolik55 <65602933+rolik55@users.noreply.github.com>
2022-08-11 11:51:41 +0300

    validation

--- main/java/mjc/beans/AbstractPurchase.java
+++ main/java/mjc/beans/AbstractPurchase.java
@@ -1,13 +1,16 @@
 package main.java.mjc.beans;
 
-import main.java.mjc.exceptions.CsvLineException;
-
-public abstract class AbstractPurchase implements Comparable<AbstractPurchase>, Cloneable {
+public abstract class AbstractPurchase implements Comparable<AbstractPurchase> {
     private final Product product;
     private final int amount;
 
-    public AbstractPurchase(String[] fields) throws Exception {
-        this(getValidPurchase(fields));
+    public AbstractPurchase(String[] fields) {
+        if (isValidPurchase(fields)) {
+            product = new Product(fields[0], Integer.parseInt(fields[1]));
+            amount = Integer.parseInt(fields[2]);
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public AbstractPurchase(AbstractPurchase purchase) {
@@ -15,13 +18,11 @@ public abstract class AbstractPurchase implements Comparable<AbstractPurchase>, 
     }
 
     public AbstractPurchase(Product product, int amount) {
-        this.product = product;
-        this.amount = amount;
+        this(new String[]{product.getName(), String.valueOf(product.getPrice().getValue()), String.valueOf(amount)});
     }
 
     public AbstractPurchase(String name, int price, int amount) {
-        this.product = new Product(name, price);
-        this.amount = amount;
+        this(new Product(name, price), amount);
     }
 
     protected abstract Euro getFinalCost(Euro baseCost);
@@ -43,34 +44,33 @@ public abstract class AbstractPurchase implements Comparable<AbstractPurchase>, 
 
     @Override
     public int compareTo(AbstractPurchase purchase) {
-        return this.getCost().compareTo(purchase.getCost());
+        return -getCost().compareTo(purchase.getCost());
     }
 
     public int getAmount() {
         return amount;
     }
 
-    static AbstractPurchase getValidPurchase(String[] fields) throws Exception {
-        if (fields.length == 4 && Integer.parseInt(fields[1]) - Integer.parseInt(fields[3]) <= 0) {
-            throw new CsvLineException();
-        }
-        for (int i = 0; i < fields.length; i++) {
-            if (i != 0 && fields[i].matches("[a-zA-Z]+")) {
-                throw new CsvLineException();
-            } else if (fields[i].isEmpty() || fields[i].equals("0") || fields[i].contains("-") || fields[i].contains(".")) {
-                throw new CsvLineException();
-            }
-        }
-        return new AbstractPurchase(fields[0], Integer.parseInt(fields[1]), Integer.parseInt(fields[2])) {
-            @Override
-            protected Euro getFinalCost(Euro baseCost) {
-                return baseCost;
-            }
-        };
+    public Product getProduct() {
+        return product;
     }
 
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
+    private static boolean isValidPurchase(String[] fields) {
+        if (fields.length == 3 || fields.length == 4) {
+            int price = Integer.parseInt(fields[1]);
+            int amount = Integer.parseInt(fields[2]);
+            if (fields.length == 4) {
+                int discount = Integer.parseInt(fields[3]);
+                if (discount == 0 || discount >= price) {
+                    throw new IllegalArgumentException();
+                }
+            }
+            if (price == 0 || amount == 0) {
+                throw new IllegalArgumentException();
+            }
+            return true;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- main/java/mjc/beans/Product.java
+++ main/java/mjc/beans/Product.java
@@ -5,6 +5,17 @@ public class Product {
     private final Euro price;
 
     public Product(String name, int price) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+        this.price = new Euro(price);
+    }
+
+    public Product(String name, String price) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         this.name = name;
         this.price = new Euro(price);
     }
@@ -16,5 +27,9 @@ public class Product {
     @Override
     public String toString() {
         return name + ";" + price;
+    }
+
+    public String getName() {
+        return name;
     }
 }
